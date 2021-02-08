7442455044d80dea621ef0027f17ad7f7636d1a1
hyeweon <berry_na@khu.ac.kr>
2021-02-08 11:04:25 +0900

    set parameter name

--- game.js
+++ game.js
@@ -1,21 +1,41 @@
-var p01 = document.querySelector('.scene01');
-var p02 = document.querySelector('.scene02');
-var b01 = document.querySelector('.btn01');
+var s01 = document.querySelector('.scene01');
+var s02 = document.querySelector('.scene02');
+var b0101 = document.querySelector('.btn0101');
 
-var x = "너 자전거 타고 싶다고 그랬잖아.";
 var i = 0;
-function typeWriter() {
-  if (i < x.length) {
-    document.getElementById("typing").innerHTML += x[i];
+var x0101 = "지이잉..Zz";
+function typeWriter0101() {
+  if (i < x0101.length) {
+    document.getElementById("typing0101").innerHTML += x0101[i];
     i++;
-    setTimeout(typeWriter, 100);
+    setTimeout(typeWriter0101, 100);
+  }
+  else{
+    b0101.style.display = 'block';
   }
 }
-function btnshow(){
-  b01.style.display = 'block';
+
+var x0201 = "너 학교 가고 있어?";
+function typeWriter0201() {
+  if (i < x0201.length) {
+    document.getElementById("typing0201").innerHTML += x0201[i];
+    i++;
+    setTimeout(typeWriter0201, 100);
+  }
 }
-typeWriter();
-setTimeout(btnshow, 2000);
+
+function btn0101(){
+  b0101.style.display = 'block';
+}
+
+function scene02(){
+  s01.style.display = 'none';
+  s02.style.display = 'block';
+  i=0;
+  typeWriter0201();
+}
+
+typeWriter0101();
 
 var y = "어쩌고 저쩌고";
 var j = 0;
@@ -25,10 +45,4 @@ function typeWriter02() {
     j++;
     setTimeout(typeWriter02, 100);
   }
-}
-
-function next01() {
-	p01.style.display = 'none';
-  p02.style.display = 'block';
-  typeWriter02();
 }
